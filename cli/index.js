820cca39d8a2ea9c3c09abb3c2e32a512bbbe6d8
DengYun <tdzl2003>
2016-01-21 23:37:55 +0800

    working

--- cli/index.js
+++ cli/index.js
@@ -2,10 +2,14 @@
  * Created by tdzl2_000 on 2015-12-18.
  */
 
-import { startServer, addFilter, PLUGINS } from 'ucms-core';
+import {
+  startServer,
+  addFilter,
+  PLUGINS,
+  SESSION,
+} from 'ucms-core';
 import * as path from 'path';
 import * as fs from 'fs';
-import assert from 'assert';
 
 global.__DEV__ = process.env.NODE_ENV !== 'production';
 
@@ -20,17 +24,22 @@ function doPiping() {
 }
 
 function loadPluginConfig() {
-  const configPath = path.join(__dirname, '../config.json');
+  const configPath = process.env.UCMS_CONFIG || path.join(__dirname, '../config.json');
   if (fs.existsSync(configPath)) {
     return JSON.parse(fs.readFileSync(configPath));
   }
 }
 
-async function main(){
+async function loadToken(ctx) {
+  return ctx.header['x-accesstoken'];
+}
+
+async function main() {
   const port = (process.env.UCMS_API_PORT | 0) || 8901;
-  const host = (process.env.UCMS_API_HOST | 0) || (__DEV__ ? 'localhost' : undefined);
+  const host = process.env.UCMS_API_HOST || (__DEV__ ? 'localhost' : undefined);
 
   addFilter(PLUGINS.LOAD_CONFIG, loadPluginConfig);
+  addFilter(SESSION.LOAD_TOKEN, loadToken);
   await startServer(port, host);
 }
 
@@ -38,6 +47,7 @@ async function main(){
 if (doPiping()) {
   main()
     .catch(err => {
-      setTimeout(()=>{throw err;});
+      console.error(err.stack);
+      setTimeout(() => {throw err;});
     });
 }
