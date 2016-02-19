fadcab318cde91e419125c6e458ba21ef89ced24
tdzl2003 <tdzl2003@gmail.com>
2016-02-19 15:59:34 +0800

    remove babelrc in each plugin & fix eslint errors.

--- bin/ucms.js
+++ bin/ucms.js
@@ -5,19 +5,41 @@
 var fs = require('fs');
 var path = require('path');
 
+var plugins = [
+  "syntax-object-rest-spread",
+  "transform-es2015-arrow-functions",
+  "transform-async-to-generator",
+  "transform-es2015-modules-commonjs",
+  "transform-es2015-destructuring",
+  "transform-object-rest-spread",
+  "transform-es2015-parameters"
+];
+
 require('babel-register')({
-  ignore: filename => {
-    var dir = path.dirname(filename);
-    for (;dir !== path.dirname(dir); dir = path.dirname(dir)) {
-      if (path.basename(dir) === 'node_modules') {
-        break;
-      }
-      if (fs.existsSync(path.join(dir, '.babelrc'))) {
-        return false;
-      }
+  plugins: plugins.map(function (plugin) {
+    // Normalise plugin to an array.
+    if (!Array.isArray(plugin)) {
+      plugin = [plugin];
     }
-    return true;
-  },
+    // Only resolve the plugin if it's a string reference.
+    if (typeof plugin[0] === 'string') {
+      plugin[0] = require(`babel-plugin-${plugin[0]}`);
+      plugin[0] = plugin[0].__esModule ? plugin[0].default : plugin[0];
+    }
+    return plugin;
+  }),
+  //ignore: filename => {
+  //  var dir = path.dirname(filename);
+  //  for (;dir !== path.dirname(dir); dir = path.dirname(dir)) {
+  //    if (path.basename(dir) === 'node_modules') {
+  //      break;
+  //    }
+  //    if (fs.existsSync(path.join(dir, '.babelrc'))) {
+  //      return false;
+  //    }
+  //  }
+  //  return true;
+  //},
 });
 
 require('../cli');
